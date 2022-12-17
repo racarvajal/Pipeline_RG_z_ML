094243f219eb495974fe9eef07fb107a27ca0c91
racarvajal <racarvajal@outlook.com>
2022-12-17 22:33:42 +0000

    new radius 1_1 arcsec

--- global_variables.py
+++ global_variables.py
@@ -35,10 +35,15 @@ mqc_version        = '7_4d'  # '7_2'
 # file_COSMOS         = 'CatWISE2020_COSMOS_MILLIQUAS_7_4d_COSMOSVLA3_PS1_GALEX_TGSS_VLASS_XMM_2MASS_ALLWISE_SDSS_DR16_5sigma_imp.h5'
 # file_S82_Ananna_17  = 'CatWISE2020_S82_VLASS_VLAS82_PS1_GALEX_TGSS_XMM_2MASS_MILLIQUAS_7_4d_ALLWISE_SDSS_DR16_Ananna_17_zsp_5sigma_imp.h5'
 # New Catalogue file names
-fits_HETDEX         = 'CatWISE2020_LoTSS_area_large_LoTSS_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_5arcsec.fits'  # 15136878 objects (1.5e7)
-fits_S82            = 'CatWISE2020_VLAS82_area_large_VLAS82_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_5arcsec.fits'  # 3590306 objects
+# fits_HETDEX         = 'CatWISE2020_LoTSS_area_large_LoTSS_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_5arcsec.fits'  # 15136878 objects (1.5e7)
+# fits_S82            = 'CatWISE2020_VLAS82_area_large_VLAS82_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_5arcsec.fits'  # 3590306 objects
+# fits_COSMOS         = ''
+# fits_S82_Ananna_17  = 'CatWISE2020_VLAS82_area_large_VLAS82_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_5arcsec_Ananna_17.fits'  # 2558 objects
+# New Catalogue file names
+fits_HETDEX         = 'CatWISE2020_LoTSS_area_large_LoTSS_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_1_1arcsec.fits'  # 15136878 objects (1.5e7)
+fits_S82            = 'CatWISE2020_VLAS82_area_large_VLAS82_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_1_1arcsec.fits'  # 3590306 objects
 fits_COSMOS         = ''
-fits_S82_Ananna_17  = 'CatWISE2020_VLAS82_area_large_VLAS82_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_5arcsec_Ananna_17.fits'  # 2558 objects
+fits_S82_Ananna_17  = 'CatWISE2020_VLAS82_area_large_VLAS82_PS1_ALLWISE_2MASS_MQC_74D_SDSS_DR16_1_1arcsec_Ananna_17.fits'  # 804 objects
 # Non imputed h5 files
 file_non_imp_HETDEX = fits_HETDEX.replace('.fits', '_non_imp.h5')
 file_non_imp_S82    = fits_S82.replace('.fits', '_non_imp.h5')
@@ -73,11 +78,17 @@ area_COSMOS         = 4  # Not real value. Placeholder
 # full_z_model       = 'regression_z_dec_10_2022'
 # high_z_model       = 'regression_high_z_dec_11_2022'
 
-star_model         = 'classification_star_no_star_dec_12_2022'
-AGN_gal_model      = 'classification_AGN_galaxy_dec_13_2022'
-radio_model        = 'classification_LOFAR_detect_dec_14_2022'
-full_z_model       = 'regression_z_dec_15_2022'
-high_z_model       = 'regression_high_z_dec_16_2022'
+# star_model         = 'classification_star_no_star_dec_12_2022'
+# AGN_gal_model      = 'classification_AGN_galaxy_dec_13_2022'
+# radio_model        = 'classification_LOFAR_detect_dec_14_2022'
+# full_z_model       = 'regression_z_dec_15_2022'
+# high_z_model       = 'regression_high_z_dec_16_2022'
+
+star_model         = 'classification_star_no_star_dec_17_2022'
+AGN_gal_model      = 'classification_AGN_galaxy_dec_18_2022'
+radio_model        = 'classification_LOFAR_detect_dec_19_2022'
+full_z_model       = 'regression_z_dec_20_2022'
+high_z_model       = 'regression_high_z_dec_121_2022'
 # Calibrated models
 cal_str_model      = 'cal_' + star_model    + '.joblib'
 cal_AGN_gal_model  = 'cal_' + AGN_gal_model + '.joblib'
@@ -99,17 +110,26 @@ naive_radio_thresh = 0.5
 # AGN_thresh         = 0.4347533096
 # radio_thresh       = 0.4999910122
 # PR-optimised models (with train+test sub-set) new data
+# star_thresh        = 0.1873511777 # old value
+# AGN_thresh         = 0.4933696292
+# radio_thresh       = 0.1253210681
+# 
 star_thresh        = 0.1873511777 # old value
-AGN_thresh         = 0.4933696292
-radio_thresh       = 0.1253210681
+AGN_thresh         = 0.5000115951
+radio_thresh       = 0.9815369877
+
 # Calibrated and PR-optimised models (with calibration sub-set)
 # cal_str_thresh     = 0.6007345636412931
 # cal_AGN_thresh     = 0.39889114096089423
 # cal_radio_thresh   = 0.3174334601810781
 # Calibrated and PR-optimised models (with calibration sub-set) new data
+# cal_str_thresh     = 0.6007345636412931 # old value
+# cal_AGN_thresh     = 0.40655238948425537
+# cal_radio_thresh   = 0.20125615854582377
+#
 cal_str_thresh     = 0.6007345636412931 # old value
-cal_AGN_thresh     = 0.40655238948425537
-cal_radio_thresh   = 0.20125615854582377
+cal_AGN_thresh     = 0.34895396724527294
+cal_radio_thresh   = 0.2046047064139296
 # High redshift limit
 high_z_limit       = 2.0  # 3.6
 
