2fd79a64b0fc9ea14b59caf22e1d9263696f7628
racarvajal <rcarvajalp@outlook.com>
2024-01-23 18:20:49 +0000

    plot moc with background

--- plot_moc_background_files.py
+++ plot_moc_background_files.py
@@ -7,9 +7,14 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from mocpy import MOC, WCS
+import numpy as np
+# from mocpy import MOC, WCS
+from mocpy import MOC
+from astropy.wcs import WCS
 from astropy.coordinates import Angle, SkyCoord
+from astropy.visualization import simple_norm
 from astropy import units as u
+from astropy.io import fits
 import colorcet as cc
 import cmasher as cmr
 import global_variables as gv
@@ -18,7 +23,7 @@ mpl.rcdefaults()
 plt.rcParams['text.usetex'] = True
 
 save_flag   = False
-show_flag   = True
+show_flag   = False
 
 used_area   = 'HETDEX'  # 'HETDEX', 'S82', 'COSMOS'
 
@@ -51,18 +56,27 @@ field_fsize  = field_fsizes[used_area]
 moc = MOC.load(gv.moc_path + file_name, 'fits')
 moc = moc.degrade_to_order(8)
 
+# load unWISE cutout covering the respective areas
+unWISE_HETDEX = 'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?hips=CDS%2FP%2FunWISE%2Fcolor-W2-W1W2-W1&width=2000&height=630&fov=50&projection=AIT&coordsys=icrs&rotation_angle=0.0&ra=195.60533258333334&dec=52.75&format=fits'
+unWISE_S82    = 'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?hips=CDS%2FP%2FunWISE%2Fcolor-W2-W1W2-W1&width=2000&height=200&fov=65&projection=AIT&coordsys=icrs&rotation_angle=0.0&ra=3.2827360833333334&dec=-0.12713066666666667&format=fits'
+unWISE_COSMOS = 'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?hips=CDS%2FP%2FunWISE%2Fcolor-W2-W1W2-W1&width=1000&height=1000&fov=2.1&projection=AIT&coordsys=icrs&rotation_angle=0.0&ra=150.10991254166663&dec=2.2293776944444446&format=fits'
+
+field_unWISE = {'HETDEX': unWISE_HETDEX, 'S82': unWISE_S82, 'COSMOS': unWISE_COSMOS}
+with fits.open(field_unWISE[used_area]) as hdul:
+    # create WCS from unWISE image header
+    unwise_wcs  = WCS(header=hdul[0].header).dropaxis(2)
+    data_unWISE = np.transpose(hdul[0].data, (1, 2, 0))
+
 fig = plt.figure(111, figsize=field_fsize)
-with WCS(fig, 
-        fov=field_fov,
-        center=field_center,
-        coordsys='icrs',
-        rotation=Angle(0, u.degree),
-        projection='AIT') as wcs:
-    ax = fig.add_subplot(1, 1, 1, projection=wcs)
-    # Call fill with a matplotlib ax and the `~astropy.wcs.WCS` wcs object.
-    moc.fill(ax=ax, wcs=wcs, alpha=1.0, fill=True, color=plt.get_cmap('cet_gouldian')(0.5), linewidth=1.0)
-    moc.border(ax=ax, wcs=wcs, alpha=1.0, color='k', lw=1.5)
-# plt.title(used_area, size=12)
+ax = fig.add_subplot(1, 1, 1, projection=unwise_wcs)
+im = ax.imshow(
+    data_unWISE,
+    origin='lower',
+    norm=simple_norm(data_unWISE, 'log', min_percent=0, max_percent=97),
+)
+moc.fill(ax=ax, wcs=unwise_wcs, alpha=0.6, 
+color='w', fill=True, linewidth=1.0)
+moc.border(ax=ax, wcs=unwise_wcs, alpha=1.0, color='k', lw=1.5)
 plt.grid(color='k', linestyle='dashed', alpha=0.5)
 ax.tick_params(which='major', direction='in')
 ax.tick_params(axis='both', which='major', labelsize=12)
@@ -73,7 +87,7 @@ ax.set_ylabel('$\mathrm{Dec}$', size=12)
 plt.setp(ax.spines.values(), linewidth=2.5)
 plt.tight_layout()
 if save_flag:
-    plt.savefig(gv.plots_path + f'field_area_moc_{used_area}.pdf')
+    plt.savefig(gv.plots_path + f'field_unWISE_back_area_moc_{used_area}.pdf', bbox_inches='tight')
 if show_flag:
     plt.show()
 if not show_flag:
