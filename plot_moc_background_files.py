69b403528cf759801d6524acbd96906606187cf5
racarvajal <rcarvajalp@outlook.com>
2024-01-25 18:06:49 +0000

    use astroquery

--- plot_moc_background_files.py
+++ plot_moc_background_files.py
@@ -8,10 +8,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import numpy as np
+from astroquery.hips2fits import hips2fits
 # from mocpy import MOC, WCS
 from mocpy import MOC
 from astropy.wcs import WCS
-from astropy.coordinates import Angle, SkyCoord
+from astropy.coordinates import Angle, SkyCoord, Longitude, Latitude
 from astropy.visualization import simple_norm
 from astropy import units as u
 from astropy.io import fits
@@ -37,23 +38,35 @@ center_HETDEX = SkyCoord('13:02:25.27982 +52:45:00.0000', unit=(u.hourangle, u.d
 center_S82    = SkyCoord('00:13:07.85666 -00:07:37.6704', unit=(u.hourangle, u.deg), frame='icrs')
 center_COSMOS = SkyCoord('10:00:26.37901 +02:13:45.7597', unit=(u.hourangle, u.deg), frame='icrs')
 
-fov_HETDEX    = 20 * u.deg
-fov_S82       = 20 * u.deg
-fov_COSMOS    = 2.2  * u.deg
+fov_HETDEX    = 50 * u.deg
+fov_S82       = 65 * u.deg
+fov_COSMOS    = 2.1  * u.deg
 
 figsize_HETDEX = (8, 3.5)
 figsize_S82    = (10, 3)
 figsize_COSMOS = (5.6, 4.5)
 
-file_names   = {'HETDEX': file_HETDEX, 'S82': file_S82, 'COSMOS': file_COSMOS}
-center_names = {'HETDEX': center_HETDEX, 'S82': center_S82, 'COSMOS': center_COSMOS}
-field_fovs   = {'HETDEX': fov_HETDEX, 'S82': fov_S82, 'COSMOS': fov_COSMOS}
-field_fsizes = {'HETDEX': figsize_HETDEX, 'S82': figsize_S82, 'COSMOS': figsize_COSMOS}
+width_HETDEX   = 2000
+width_S82      = 2000
+width_COSMOS   = 1000
+
+height_HETDEX   = 630
+height_S82      = 200
+height_COSMOS   = 1000
+
+file_names    = {'HETDEX': file_HETDEX, 'S82': file_S82, 'COSMOS': file_COSMOS}
+center_names  = {'HETDEX': center_HETDEX, 'S82': center_S82, 'COSMOS': center_COSMOS}
+field_fovs    = {'HETDEX': fov_HETDEX, 'S82': fov_S82, 'COSMOS': fov_COSMOS}
+field_fsizes  = {'HETDEX': figsize_HETDEX, 'S82': figsize_S82, 'COSMOS': figsize_COSMOS}
+field_widths  = {'HETDEX': width_HETDEX, 'S82': width_S82, 'COSMOS': width_COSMOS}
+field_heights = {'HETDEX': height_HETDEX, 'S82': height_S82, 'COSMOS': height_COSMOS}
 
 file_name    = file_names[used_area]
 field_center = center_names[used_area]
 field_fov    = field_fovs[used_area]
 field_fsize  = field_fsizes[used_area]
+field_width  = field_widths[used_area]
+field_height = field_heights[used_area]
 
 moc = MOC.load(gv.moc_path + file_name, 'fits')
 moc = moc.degrade_to_order(8)
@@ -62,22 +75,31 @@ moc = moc.degrade_to_order(8)
 # could use unWISE colours: 'color-W2-W1W2-W1'
 # or one single unWISE band: 'W1'
 band_2_plot = 'W1' # 'color-W2-W1W2-W1', 'W1'
-unWISE_HETDEX = f'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?hips=CDS%2FP%2FunWISE%2F{band_2_plot}&width=2000&height=630&fov=50&projection=AIT&coordsys=icrs&rotation_angle=0.0&ra=195.60533258333334&dec=52.75&format=fits'
-unWISE_S82    = f'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?hips=CDS%2FP%2FunWISE%2F{band_2_plot}&width=2000&height=200&fov=65&projection=AIT&coordsys=icrs&rotation_angle=0.0&ra=3.2827360833333334&dec=-0.12713066666666667&format=fits'
-unWISE_COSMOS = f'https://alasky.cds.unistra.fr/hips-image-services/hips2fits?hips=CDS%2FP%2FunWISE%2F{band_2_plot}&width=1000&height=1000&fov=2.1&projection=AIT&coordsys=icrs&rotation_angle=0.0&ra=150.10991254166663&dec=2.2293776944444446&format=fits'
+hips        = f'CDS/P/unWISE/{band_2_plot}'
 
-field_unWISE = {'HETDEX': unWISE_HETDEX, 'S82': unWISE_S82, 'COSMOS': unWISE_COSMOS}
-with fits.open(field_unWISE[used_area]) as hdul:
-    # create WCS from unWISE image header
-    if band_2_plot == 'color-W2-W1W2-W1':
-        unwise_wcs  = WCS(header=hdul[0].header).dropaxis(2)
-        data_unWISE = np.transpose(hdul[0].data, (1, 2, 0))
-    if band_2_plot == 'W1':
-        unwise_wcs  = WCS(header=hdul[0].header)
-        data_unWISE = hdul[0].data
-    # to compute skycoords for every pixel of the image
-    width  = hdul[0].header["NAXIS1"]
-    height = hdul[0].header["NAXIS2"]
+query_unWISE = hips2fits.query(
+    hips=hips,
+    width=field_width,
+    height=field_height,
+    ra=Longitude(field_center.ra),
+    dec=Latitude(field_center.dec),
+    fov=field_fov,
+    projection='AIT',
+    get_query_payload=False,
+    format='fits',
+    verbose=True
+)
+
+# create WCS from unWISE image header
+if band_2_plot == 'color-W2-W1W2-W1':
+    unwise_wcs  = WCS(header=query_unWISE[0].header).dropaxis(2)
+    data_unWISE = np.transpose(query_unWISE[0].data, (1, 2, 0))
+if band_2_plot == 'W1':
+    unwise_wcs  = WCS(header=query_unWISE[0].header)
+    data_unWISE = query_unWISE[0].data
+# to compute skycoords for every pixel of the image
+width  = query_unWISE[0].header["NAXIS1"]
+height = query_unWISE[0].header["NAXIS2"]
 
 if not plot_all_area:
     xv, yv      = np.meshgrid(np.arange(0, width), np.arange(0, height))
@@ -113,7 +135,7 @@ ax.tick_params(axis='both', which='major', labelsize=12)
 ax.tick_params(which='major', length=0, width=1.5)
 ax.tick_params(which='minor', length=0)
 ax.set_xlabel('$\mathrm{R.A.}$', size=12)
-ax.set_ylabel('$\mathrm{Dec}$', size=12)
+ax.set_ylabel('$\mathrm{Declination}$', size=12)
 plt.setp(ax.spines.values(), linewidth=2.5)
 plt.tight_layout()
 if save_flag:
